596d6aba7df4c7c3ad4ccb763bb4f01185b37339
Flames <vibe-coder@flames.com>
2025-11-19 12:23:27 +0000

    Update project prj_mnaqyn8u - 2025-11-19T12:23:26.613Z

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Test from './Test'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
 import './index.css'
+
+function Layout({ children }){
+  return children
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/test" element={<Test />} />
+        <Route path="/about" element={<Layout><About /></Layout>} />
+        <Route path="/services" element={<Layout><Services /></Layout>} />
+        <Route path="/contact" element={<Layout><Contact /></Layout>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
